ad510dd53722f6714204f73789fb160adf03c757
Kushagra Gupta <guptakushagra1404@gmail.com>
2024-08-20 21:51:42 +0530

    added projects details

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,21 +5,24 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias eaque modi inventore veniam pariatur, commodi odio vel nihil. Repellat ad beatae doloribus ab labore accusamus rerum aperiam tempora sunt sequi.",
+    title: "GizmoStore",
+    img: "gizmoStore.png",
+    desc:"Built a feature-rich E-commerce platform with a MERN stack, including a powerful admin dashboard and secure payment gateway integration. This scalable solution ensures a seamless and secure shopping experience for users, combining efficient product management with smooth transaction processing.",
+    href:"https://gizmostore.netlify.app/",
   },
   {
     id: 2,
-    title: "Nextjs Commerce",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias eaque modi inventore veniam pariatur, commodi odio vel nihil. Repellat ad beatae doloribus ab labore accusamus rerum aperiam tempora sunt sequi.",
+    title: "AlgorythmPlay",
+    img: "algoPlay.png",
+    desc: "AlgorythmPlay is an interactive platform that includes an algorithm visualizer and games for various sorting algorithms and N-Queens. Users can explore and understand algorithms visually and enhance problem-solving skills through interactive games. This project provides an engaging and educational experience for learning and practicing various algorithms.",
+    href: "https://algorythm-play.netlify.app/",
   },
   {
     id: 3,
-    title: "Vanillajs Commerce",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias eaque modi inventore veniam pariatur, commodi odio vel nihil. Repellat ad beatae doloribus ab labore accusamus rerum aperiam tempora sunt sequi.",
+    title: "Portfolio Website",
+    img: "portfolio.png",
+    desc: "Dynamic portfolio website made using React, highlighting MERN stack projects and various other creations. Implemented user-friendly features for email connection and resume download, enhancing engagement.",
+    href: "",
   },
   // {
   //   id: 4,
@@ -33,21 +36,21 @@ const items = [
 const Single = ({ item }) => {
   const ref = useRef();
   const { scrollYProgress } = useScroll({ target: ref });
-  //its value depends upon 
-  const y= useTransform(scrollYProgress,[0,1],[-300,300]);
+  //its value depends upon
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
   //0->-300px && 1->300px
   return (
     <section>
       <div className="container">
         <div className="wrapper">
-            <div className="imageContainer" ref={ref}>
-                <img src={item.img} alt="" />
-            </div>
-            <motion.div className="textContainer" style={{y:y}}>
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y: y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
-            </motion.div>
+            <button><a href={item.href}>See Demo</a></button>
+          </motion.div>
         </div>
       </div>
     </section>
